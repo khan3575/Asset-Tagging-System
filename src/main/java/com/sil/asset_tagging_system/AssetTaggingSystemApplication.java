22c6d4abd64df810126799ee0179e96def6c0563
sakibkhan <sakibkhan.stud@gmail.com>
2026-07-08 16:32:59 +0600

    first commit

---
+++ src/main/java/com/sil/asset_tagging_system/AssetTaggingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sil.asset_tagging_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetTaggingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetTaggingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sil/asset_tagging_system/AssetTaggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sil.asset_tagging_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTaggingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
