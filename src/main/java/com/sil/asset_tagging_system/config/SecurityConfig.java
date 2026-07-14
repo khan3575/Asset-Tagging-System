4bdeab38aee54300136acc32c6d3bf03f72a7239
sakibkhan <sakibkhan.stud@gmail.com>
2026-07-14 13:11:43 +0600

    added user authentication config -added customUserDetails -added customUserService -added GlobalException -updated security to add Authentication Manager

--- src/main/java/com/sil/asset_tagging_system/config/SecurityConfig.java
+++ src/main/java/com/sil/asset_tagging_system/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.sil.asset_tagging_system.config;
 import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,6 +52,12 @@ public class SecurityConfig {
         return new HttpSessionSecurityContextRepository();
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+    {
+        return config.getAuthenticationManager();
+    }
     // needs to add cors for the front end or not sure how does manual origin is handled...
 
 }
