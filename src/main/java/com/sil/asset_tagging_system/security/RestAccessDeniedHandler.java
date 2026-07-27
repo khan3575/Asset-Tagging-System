ad163a435086eb8bff7dd1145232fb2e0012895a
sakibkhan <sakibkhan.stud@gmail.com>
2026-07-27 12:50:20 +0600

    reverted back to spring security default setting to let spring securities builtin function to handle the login

--- src/main/java/com/sil/asset_tagging_system/security/RestAccessDeniedHandler.java
+++ src/main/java/com/sil/asset_tagging_system/security/RestAccessDeniedHandler.java
@@ -1,30 +1,20 @@
 package com.sil.asset_tagging_system.security;
 
-import com.sil.asset_tagging_system.dto.AuthResponseDTO;
-import com.sil.asset_tagging_system.dto.response.ApiResponse;
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
 
 @Component
 public class RestAccessDeniedHandler implements AccessDeniedHandler {
-    private final ObjectMapper objectMapper ;
-    RestAccessDeniedHandler(ObjectMapper objectMapper)
-    {
-        this.objectMapper = objectMapper;
-    }
+    
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        response.setContentType("application/json");
-        ApiResponse<AuthResponseDTO> apiResponse = ApiResponse.failure("You do not have permission to access this resource.");
-        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.sendRedirect(request.getContextPath() + "/dashboard?error");
     }
 }
