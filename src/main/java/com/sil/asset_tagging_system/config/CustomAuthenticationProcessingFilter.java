b9e02d34ee95958cc0fd2e6bc816e6d9c5a319a6
sakibkhan <sakibkhan.stud@gmail.com>
2026-07-27 11:22:04 +0600

    updated CustomAuthenticationProcessingFilter.attemptAuthentication method to handle direct username password instead of inputStream

--- src/main/java/com/sil/asset_tagging_system/config/CustomAuthenticationProcessingFilter.java
+++ src/main/java/com/sil/asset_tagging_system/config/CustomAuthenticationProcessingFilter.java
@@ -1,20 +1,17 @@
 package com.sil.asset_tagging_system.config;
  
-import com.sil.asset_tagging_system.dto.request.LoginRequest;
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import org.jspecify.annotations.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
- 
-import java.io.IOException;
 public class CustomAuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
  
  
@@ -24,12 +21,9 @@ public class CustomAuthenticationProcessingFilter extends AbstractAuthentication
  
     @Override
     public @Nullable Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
-        ObjectMapper mapper = new ObjectMapper();
-        LoginRequest loginRequest = mapper.readValue(request.getInputStream(), LoginRequest.class);
- 
-        String email = loginRequest.email();
-        String password = loginRequest.password();
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(email, password);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
         return this.getAuthenticationManager().authenticate(token);
     }
 }
