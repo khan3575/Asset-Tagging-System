f90a447e7461c5e94439adf2e791e4f58d93a726
sakibkhan <sakibkhan.stud@gmail.com>
2026-08-03 11:09:16 +0600

    added routing for audit log

---
+++ src/main/java/com/sil/asset_tagging_system/controller/AuditLogViewController.java
@@ -0,0 +1,16 @@
+package com.sil.asset_tagging_system.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuditLogViewController {
+
+    @GetMapping("/audit-log")
+    public void viewAuditLog(HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        request.getRequestDispatcher("/audit-log.xhtml").forward(request,response);
+    }
+}
