24f9c4d28a5e109268d2e482df2f6d413369c2b7
sakibkhan <sakibkhan.stud@gmail.com>
2026-08-09 12:52:31 +0600

    added ip address to the userlist

---
+++ src/main/java/com/sil/asset_tagging_system/bean/DashboardBean.java
@@ -0,0 +1,4 @@
+package com.sil.asset_tagging_system.bean;
+
+public class DashboardBean {
+}
--- src/main/java/com/sil/asset_tagging_system/security/LoginAuditListener.java
+++ src/main/java/com/sil/asset_tagging_system/security/LoginAuditListener.java
@@ -1,14 +1,16 @@
 package com.sil.asset_tagging_system.security;
 
-import com.sil.asset_tagging_system.dao.AuditLogDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.sil.asset_tagging_system.dao.AuditLogDao;
 
 @Component
 public class LoginAuditListener {
@@ -29,15 +31,22 @@ public class LoginAuditListener {
         Long userId = (principal instanceof CustomUserDetails userDetails) ? userDetails.getUserId(): null;
         String email = event.getAuthentication().getName();
         log.info("Login success for email : {} , user_id : {} ", email, userId);
-        auditLogDao.log(userId, "LOGIN_SUCCESS", "AUTH", userId, "login : "+email, null);
+        auditLogDao.log(userId, "LOGIN_SUCCESS", "AUTH", userId, "login : "+email, extractIp(event.getAuthentication()));
     }
 
     @EventListener
     public void onFailure(AbstractAuthenticationFailureEvent event)
     {
         String attemptedEmail = event.getAuthentication().getName();
+
         log.warn("Login failed for email : {}", attemptedEmail);
-        auditLogDao.log(null, "LOGIN_FAILURE", "AUTH", null, "Failed login attempt : "+attemptedEmail, null);
+        auditLogDao.log(null, "LOGIN_FAILURE", "AUTH", null, "Failed login attempt : "+attemptedEmail, extractIp(event.getAuthentication()));
     }
+
+    public String extractIp(Authentication auth)
+    {
+        return (auth.getDetails() instanceof WebAuthenticationDetails webAuth) ? webAuth.getRemoteAddress() : "";
+    }
+
 
 }
