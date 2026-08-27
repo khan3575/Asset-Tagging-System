4890b618383146fddbcb20b01c9dcef7098a7d89
sakibkhan <sakibkhan.stud@gmail.com>
2026-08-27 11:27:45 +0600

    same url redirection bug on user
    -userControler @path variable changed Long -> String
    - removed @PostConstruct as the f:viewAction load the id
    - and removed manual fetch

--- src/main/java/com/sil/asset_tagging_system/bean/UserDetailBean.java
+++ src/main/java/com/sil/asset_tagging_system/bean/UserDetailBean.java
@@ -1,17 +1,15 @@
 package com.sil.asset_tagging_system.bean;
 
-import com.sil.asset_tagging_system.dao.UserDao;
-import com.sil.asset_tagging_system.model.User;
-import com.sil.asset_tagging_system.model.enums.RoleName;
-import com.sil.asset_tagging_system.util.FacesUtil;
-import com.sil.asset_tagging_system.util.OptionalUtils;
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import lombok.Getter;
 
-import java.util.Map;
+import com.sil.asset_tagging_system.dao.UserDao;
+import com.sil.asset_tagging_system.model.User;
+import com.sil.asset_tagging_system.model.enums.RoleName;
+import com.sil.asset_tagging_system.util.OptionalUtils;
+
+import lombok.Getter;
 
 @Getter
 @Named
@@ -28,26 +26,8 @@ public class UserDetailBean {
     }
 
 
-    @PostConstruct
-    public void init()
-    {
-        initiateUser();
-
-    }
-
-    public void initiateUser(){
-        Map<String, String> params = FacesUtil.getRequestParams();
-
-        String idParam = params.get("id");
-
-        if( idParam==null || idParam.isBlank())
-        {
-            user = null;
-        }
-        else{
-            this.id = Long.valueOf(idParam);
-            user = OptionalUtils.orThrowDbFetch(userDao.findById(id), "User");
-        }
+    public void load(){
+        user = OptionalUtils.orThrowDbFetch(userDao.findById(id), "User");   
     }
 
     public boolean hasRole(RoleName roleName)
@@ -55,4 +35,8 @@ public class UserDetailBean {
         return user != null && user.getRoles().stream().anyMatch(role -> role.getName() == roleName);
     }
 
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
 }
--- src/main/java/com/sil/asset_tagging_system/controller/UserController.java
+++ src/main/java/com/sil/asset_tagging_system/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sil.asset_tagging_system.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ public class UserController {
         request.getRequestDispatcher("/user-list.xhtml").forward(request, response);
     }
     @GetMapping("/user/{id}")
-    public void getUser(HttpServletRequest request, HttpServletResponse response, @PathVariable Long id) throws Exception
+    public void getUser(HttpServletRequest request, HttpServletResponse response, @PathVariable String id) throws Exception
     {
         request.getRequestDispatcher("/user-detail.xhtml?id="+id).forward(request,response);
     }
